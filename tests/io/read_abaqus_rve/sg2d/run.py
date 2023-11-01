d45419f17560839a8152bdd259f64a48392b95d5
Su Tian <steventian90s@gmail.com>
2023-11-01 19:06:33 -0400

    1101.1906

--- tests/io/read_abaqus_rve/sg2d/run.py
+++ tests/io/read_abaqus_rve/sg2d/run.py
@@ -3,12 +3,14 @@ import sgio
 # fn_base = 'sg2_box_composite_section'
 # fn_base = 'sg2_plydrop_composite_section'
 # fn_base = 'sg2_airfoil_composite_section'
-fn_base = 'sg2_airfoil_2'
+# fn_base = 'sg2_airfoil_2'
+fn_base = 'sg2_iso_airfoil'
 
 fn_rve_abq_inp = f'../../../files/{fn_base}.inp'
 sg = sgio.read(fn_rve_abq_inp, file_format='abaqus')
 sg.sgdim = 2
 sg.smdim = 1
+sg.model = 1
 
 print(sg)
 # print(sg.mesh.get_cell_data('property_ref_csys', 'quad'))
