0795386fb1fe37c59411f0f22d51a2264d19e660
Su Tian <steventian90s@gmail.com>
2025-05-01 14:10:18 -0400

    fix issues of reading vabs d out and write gmsh file

--- tests/test_io_vabs_out_state_f.py
+++ tests/test_io_vabs_out_state_f.py
@@ -1,7 +1,7 @@
 import sgio
 
 ver = '4.0'
-fn_base = 'vabs40/uh60a'
+fn_base = 'files/vabs/version_4_0/uh60a'
 fn_sg = f'{fn_base}.sg'
 fn_sg_fi = f'{fn_sg}.fi'
 fn_msh = f'{fn_base}.msh'
@@ -11,11 +11,11 @@ name_sr = 'strength ratio'
 sg = sgio.read(fn_sg, 'vabs', sgdim=2, smdim=1)
 print(sg)
 
-state_case = sgio.readOutputState(fn_sg, 'v', 'fi', sg=sg, tool_ver=ver)
+state_case = sgio.readOutputState(fn_sg, 'v', 'fi', sg=sg, tool_version=ver)
 print(state_case)
 _sr = state_case.getState('fi').data
 # print(_sr)
 sgio.addCellDictDataToMesh(name_sr, _sr, sg.mesh)
 
-sgio.write(sg, fn_msh, 'gmsh', mesh_only=True)
+sgio.write(sg, fn_msh, 'gmsh', format_version='4.1', mesh_only=True)
 
